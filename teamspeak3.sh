5ba5877f9e26bc20cfe988580e3443974da4f019
DELENCRE Charles <charles.delencre.ext@hubone.fr>
2016-12-16 23:38:28 +0100

    tmp

--- teamspeak3.sh
+++ teamspeak3.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-workfile="teamspeak3_workfile.txt"
-workfile2="teamspeak3_last.txt"
+workfile='teamspeak3_workfile.txt'
+workfile2='teamspeak3_last.txt'
+jsontmp='teamspeak3.json.tmp'
 source $dir/config/teamspeak3.config 
 
 # DATAS
@@ -30,21 +31,23 @@ SLEEP=1
         echo "clientlist"
         sleep $SLEEP
         echo "quit"
-} | nc  localhost $port > "$dir/$workfile"
+} | nc  localhost $port > $dir/$workfile
 
 # NUMBER USERS
-$(sed ':a;N;$!ba;s/|/\n/g' "$dir/$workfile" | grep "client_nickname" > "$dir/$workfile2")
+$(sed ':a;N;$!ba;s/|/\n/g' $dir/$workfile | grep "client_nickname" > $dir/$workfile2)
 $(rm -f "$dir/$workfile")
-nbr=$(grep -cv $user "$dir/$workfile2")
+nbr=$(grep -cv $user $dir/$workfile2)
 
-$(cat $dir/json/teamspeak3.json > $json)
-$(sed -i "s/#{CPU}#/$cpu/g" $json)
-$(sed -i "s/#{MEM}#/$mem/g" $json)
-$(sed -i "s/#{UPTIME}#/$upt/g" $json)
-$(sed -i "s/#{USERS}#/$nbr/g" $json)
+$(cat $dir/json/teamspeak3.json > $jsontmp)
+$(sed -i "s/#{CPU}#/$cpu/g" $jsontmp)
+$(sed -i "s/#{MEM}#/$mem/g" $jsontmp)
+$(sed -i "s/#{UPTIME}#/$upt/g" $jsontmp)
+$(sed -i "s/#{USERS}#/$nbr/g" $jsontmp)
 
 for u in `grep -Po '(?<=client_nickname=).*(?=\ client_type)' $dir/$workfile2 | grep -v $user`
 do
-    $(sed -i "s/#{LIST}#/'$u',\n        #{LIST}#/g" $json)
+    $(sed -i "s/#{LIST}#/'$u',\n        #{LIST}#/g" $jsontmp)
 done
-$(sed '/        #{LIST}#/d' $json)
+$(sed -i '/        #{LIST}#/d' $jsontmp)
+
+$(mv $jsontmp $json)
