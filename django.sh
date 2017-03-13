f239630dce99451ddde0caf2e58fd46632199ef8
HicInformatic <charlesdelencre@gmail.com>
2017-03-13 16:41:45 +0100

    test

---
+++ django.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $dir/config/server.config 
+gunicorntmp='server.json.tmp'
+
+
+# CPU
+for i in `ps -u django -o %cpu --no-headers | grep -Eo '[0-9]{1,3}'`
+do
+    cpu=$(( $cpu + $i ))
+done
+
+echo $cpu
