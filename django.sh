c9b0cb903cb098d0d4efe27b8a234e344d1e686f
HicInformatic <charlesdelencre@gmail.com>
2017-03-13 16:45:35 +0100

    test

--- django.sh
+++ django.sh
@@ -9,7 +9,7 @@ for i in `ps -u django -o %cpu --no-headers | grep -Eo '[0-9]{1,3}'`
 do
     cpu=$(( $cpu + $i ))
 done
-echo $cpu
+echo $(($cpu/10))
 
 # MEM
 for i in `ps -u django -o %mem --no-headers | grep -Eo '[0-9]{1,3}'`
