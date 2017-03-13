a4d44b292da3e4324f883a8b8cd945112c9c28e3
hicinformatic <hicinformatic@gmail.com>
2017-03-14 00:55:59 +0100

    test

--- teamspeak3.sh
+++ teamspeak3.sh
@@ -5,19 +5,26 @@ workfile2='teamspeak3_last.txt'
 jsontmp='teamspeak3.json.tmp'
 source $dir/config/teamspeak3.config 
 
+$(cat $dir/json/teamspeak3.json > $jsontmp)
+
 # DATAS
 datas=($(ps -u teamspeak3 -o %cpu,%mem,etimes --no-headers))
+
+# UPTIME
+uptime=${datas[2]}
+$(sed -i "s/#{UPTIME}#/$upt/g" $jsontmp)
 
 # CPU
 cpu=($(echo ${datas[0]} | grep -Eo '[0-9]{1,3}'))
 cpu=${cpu[0]}
+$(sed -i "s/#{CPU}#/$cpu/g" $jsontmp)
 
 # MEMORY
 mem=($(echo ${datas[1]}  | grep -Eo '[0-9]{1,3}'))
 mem=${mem[0]}
+$(sed -i "s/#{MEM}#/$mem/g" $jsontmp)
 
-# UPTIME
-upt=${datas[2]}
+
 
 # WORKFILE
 SLEEP=1
@@ -36,14 +43,6 @@ SLEEP=1
 # NUMBER USERS
 $(sed ':a;N;$!ba;s/|/\n/g' $dir/$workfile | grep "client_nickname" > $dir/$workfile2)
 $(rm -f "$dir/$workfile")
-nbr=$(grep -cv $ts3user $dir/$workfile2)
-
-$(cat $dir/json/teamspeak3.json > $jsontmp)
-$(sed -i "s/#{CPU}#/$cpu/g" $jsontmp)
-$(sed -i "s/#{MEM}#/$mem/g" $jsontmp)
-$(sed -i "s/#{UPTIME}#/$upt/g" $jsontmp)
-$(sed -i "s/#{USERS}#/$nbr/g" $jsontmp)
-
 for u in `grep -Po '(?<=client_nickname=).*(?=\ client_type)' $dir/$workfile2 | grep -v $ts3user`
 do
     if [ -n "$client" ]
@@ -54,14 +53,6 @@ do
 done
 $(sed -i "s/#{LIST}#/\"$client\"/g" $jsontmp)
 $(rm -f $dir/$workfile2)
-
-if [ $cpu -gt 90 ] || [ $mem -gt 90 ] ; then
-    $(sed -i "s/#{STATUS}#/off/g" $jsontmp)
-elif [ $cpu -gt 60 ] || [ $mem -gt 60 ] ; then
-    $(sed -i "s/#{STATUS}#/warning/g" $jsontmp)
-else
-    $(sed -i "s/#{STATUS}#/on/g" $jsontmp)
-fi
 
 $(chown $user:$group $jsontmp)
 $(chmod $chmod $jsontmp)
