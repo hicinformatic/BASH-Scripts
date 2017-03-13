4e25a15fc387b99438e32aae8e007237c50b8625
hicinformatic <hicinformatic@gmail.com>
2017-03-13 20:20:57 +0100

    test django

--- django.sh
+++ django.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $dir/config/server.config 
-jsontmp='django.json.tmp'
-
+jsontmp="$user.json.tmp"
 
 $(cat $dir/json/django.json > $jsontmp)
 
+# UPTIME
+uptime=$(ps -u $user -o etimes,cmd | grep "$bind" | awk '{print $1}' | head -n1)
+$(sed -i "s/#{UPTIME}#/\"$uptime\"/g" $jsontmp)
+
 # CPU
-for c in `ps -u django -o %cpu --no-headers`
+for c in `ps -u $user -o %cpu --no-headers`
 do
     if [ -n "$pscpu" ]
     then
@@ -18,7 +21,7 @@ done
 $(sed -i "s/#{CPU}#/\"$pscpu\"/g" $jsontmp)
 
 # MEM
-for m in `ps -u django -o %mem --no-headers`
+for m in `ps -u $user -o %mem --no-headers`
 do
     if [ -n "$psmem" ]
     then
@@ -27,3 +30,6 @@ do
     psmem=$m
 done
 $(sed -i "s/#{MEM}#/\"$psmem\"/g" $jsontmp)
+
+# USERS
+$(sed -i "s/#{LIST}#/\"Gyn\"/g" $jsontmp)
